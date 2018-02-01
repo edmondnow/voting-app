1944754462772bef2e144f3b32baae824c3d7507
Edmond Bitay <edmond.bitay@gmail.com>
2018-02-01 16:39:15 +0100

    added poll creation page

---
+++ assets/dashboard.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){ 
+						   
+	$("#addoption").click(function(){
+		$("#addoption").before('<div class="option"><input class="form-control" type="text" placeholder="Option wanted here!"><i class="fa fa-times"></div>')
+	});
+
+	$('body').on('click', 'i', function(){
+   		$(this).closest('.option').remove();
+	});
+	
+});
+
